f7bfef757bd327eb60f383ed4d6449fe776ea3ca
CDTrautmann <cdtrautm@ualberta.ca>
2021-06-29 16:10:39 -0600

    firrst commit

---
+++ Calc_fcns.py
@@ -0,0 +1,44 @@
+# calculation functions
+import numpy as np
+def Ymax(q,U,k,alpha,T,T_o,d,Ro,Reg):
+    if Reg == 1:
+        y_max = np.sqrt(2*alpha*q/np.pi/np.e/k/U/(T-T_o))
+    elif Reg == 2:
+        y_max = q/2/np.pi/k/(T-T_o)
+    elif Reg == 3:
+        y_max = np.sqrt(1/2/np.pi/np.e)*q*alpha/k/d/U/(T-T_o)
+    elif Reg == 4:
+        gam = float(input('enter gamma? '))
+        y_max = 4*np.exp(-gam)*alpha/U*np.exp(-1/Ro)
+    else:
+        y_max = 'err'
+        print('regime error')
+    return(y_max)
+
+def Yhaz(q,U,k,alpha,T,T_o,d,Ro,Reg,):
+    T_m = float(input('enter melting temp '))
+    T_haz = float(input('enter HAZ temp'))
+
+    if Reg == 1:
+        y_haz = np.sqrt(q*alpha/2/np.e/np.pi/k/U)*(T_m-T_haz)/pow((T-T_o),(3/2))
+    elif  Reg == 2:
+        y_haz = (q/2/np.pi/k)*(T_m-T_haz)/(T-T_o)**2
+    elif  Reg == 3:
+        y_haz = q*alpha/np.sqrt(2*np.e*np.pi)/k/d/U*(T_m-T_haz)/(T-T_o)**2
+    elif Reg == 4:
+        gam = float(input('enter gamma '))
+        8*np.pi*k*alpha*d/q/U*(T_m-T_haz)/np.exp(gam+(1/Ro))
+    else:
+        print('regime error')
+    return(y_haz)
+
+def CoolRate(q,U,k,alpha,T,T_o,d,Ro,Reg):
+    T_m = [float(input('enter melt temperature'))]
+    if Reg == 1 or Reg == 2:
+        T_b = -2*np.pi*k*U/q*(T-T_o)^2
+    if Reg == 3:
+        T_b = -2*np.pi/alpha*((k*d*U/q)**2)*(T-T_o)**3
+    if Reg == 4:
+        gam = float(input('enter gamma '))
+        T_b = -q/8/np.pi/k/alpha/d*(U**2)*np.exp(gam+(1/Ro))
+    return(T_b)
---
+++ welding_eqn program.py
@@ -0,0 +1,79 @@
+import numpy as np
+import Calc_fcns
+
+# function to take user parameters
+def ParamIn():
+    q = float(input('enter heat input '))
+    U = float(input('enter travel speed '))
+    alpha = float(input('enter thermal diffusivity '))
+    k = float(input('enter thermal conductivity '))
+    T_o = float(input('enter ambient temperature in C '))
+    T = float(input('enter intermediate temperature '))
+    d= float(input('enter plate thickness(m) '))
+    Ry= q*U/(4*np.pi*k*alpha*(T-T_o))
+    d_s = (U*d)/(2*alpha)
+    Ro = Ry/d_s
+    return(q,U,alpha,k,T_o,T,d,Ry,d_s,Ro)
+
+# function to determine regime
+def regimeSet(Ry,d_s):
+    Reg=0
+    intermed=0
+    if Ry<10 and Ry>0.1 and d_s<10 and d_s>0.1:
+        intermed = 1 
+
+    if Ry > 1:
+        if d_s > np.sqrt(np.pi*Ry/2):
+            Reg = 1
+        else:
+            Reg = 3
+    if Ry < 1:
+        if d_s > -Ry*np.log(Ry):
+            Reg = 2
+        elif d_s > Ry:
+            Reg = 4
+        else:
+            Reg = 3
+
+    print(' ')
+    print('Ry=',Ry)
+    print('d*=',d_s)
+    print(' your regime is ',Reg)
+    if intermed == 1:
+        print('you are in an intemediate regime')
+        print('intermediate corrections not implemented')
+    print(' ')
+    return(Reg,intermed)
+
+# User selects what to calculate
+def FuncChoose():
+    print(' please enter a number to select your calculation')
+    print('[0] to end program')
+    print('[1] for HAZ width')
+    print('[2] for centerline cooling rate')
+    print('[3] for Ymax')
+    calc_pref  = 0
+    calc_pref = int(input())
+    return(calc_pref)
+
+# call the function the User chose
+def FuncCall(calc_pref,q,U,k,alpha,T_o,T,d,Ry,d_s,Ro,Reg,intermed):
+    if calc_pref == 0:
+        print('done')
+    elif calc_pref == 1:
+        y_haz = Calc_fcns.Yhaz(q,U,k,alpha,T,T_o,d,Ro,Reg)
+        print(y_haz)
+    elif calc_pref == 2:
+        Tb = Calc_fcns.CoolRate(q,U,k,alpha,T,T_o,d,Ro,Reg)
+        print(Tb)
+    elif calc_pref == 3:
+        y_max = Calc_fcns.Ymax(q,U,k,alpha,T,T_o,d,Ro,Reg)
+        print(y_max)
+
+def main():
+    q,U,alpha,k,T_o,T,d,Ry,d_s,Ro = ParamIn()
+    Reg,intermed = regimeSet(Ry,d_s)
+    calc_pref = FuncChoose()
+    FuncCall(calc_pref,q,U,alpha,k,T_o,T,d,Ry,d_s,Ro,Reg,intermed)
+
+main()
