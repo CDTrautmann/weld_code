3ef6d8f513e00ec7352273df95f408cfb67df4c5
CDTrautmann <cdtrautm@ualberta.ca>
2021-07-02 14:43:40 -0600

    Added extra calculatoins, improved error handling, and reformatted window

--- TknterPractice.py
+++ TknterPractice.py
@@ -11,19 +11,23 @@ weldp = {}
 y_haz =0
 Reg = 0
 intermed = 0
+
 reg_var = tk.StringVar()
 Yhaz_var = tk.StringVar()
+Ymax_var = tk.StringVar()
+Cool_var = tk.StringVar()
+
 labels = ['q','U','alpha','k','T_o','T','d',]
 variables = []
 entries = []
 
 for i,nam in enumerate(labels):
-    en = tk.Entry(entrywindow)
+    en=tk.Entry(entrywindow)
     en.grid(row=i+1,column=1)
     entries.append(en)
 
 for i,nam in enumerate(labels):
-    lm = tk.Label(entrywindow, text = nam)
+    lm=tk.Label(entrywindow,text=nam)
     lm.grid(row = i+1)
 
 def regimeset():
@@ -60,49 +64,127 @@ def regimeset():
             Reg = 4
         else:
             Reg = 3
-    messagebox.showinfo(title='Regime',message = Reg)
+    #messagebox.showinfo(title='Regime',message = Reg)
     reg_var.set(str(Reg))
 
 
-Reg_button = tk.Button(entrywindow, text = 'regime set',command = regimeset).grid(row=9,column=0)
-Reg_value = tk.Label(entrywindow, textvariable=reg_var ).grid(row = 1, column = 5 )
-Reg_label = tk.Label(entrywindow, text='Regime: ').grid(row=1,column=4)
+Reg_button=tk.Button(entrywindow,text='regime set',command=regimeset).grid(row=9,column=0)
+Reg_value=tk.Label(entrywindow,textvariable=reg_var ).grid(row=1,column=3)
+Reg_label=tk.Label(entrywindow,text='Regime: ').grid(row=1,column=2)
 
 def YhazButton():
+
+    y_window = tk.Tk()
+    param_in = ['T_m',"T_haz"]
+    Local_entry = []
+    T_holder = {}
+    try:
+        int(reg_var.get())
+    except:
+        messagebox.showerror(title='Error',message='Please calculate Regime first')
+        y_window.destroy()
+    else:
+        for i,named in enumerate(param_in):
+            en=tk.Entry(y_window)
+            en.grid(row=i+1,column=1)
+            Local_entry.append(en)
+
+    for i,named in enumerate(param_in):
+        lm=tk.Label(y_window,text=named)
+        lm.grid(row=i+1,column=2)
+
     def Ycalc():
         y_haz = 0
-        try:
-            print(Tm_in.get())
-            float(Tm_in.get())
-        except:
-            messagebox.showerror(title='Type error',message='Enter numbers only')
-        else:
-            T_m = float(Tm_in.get())
-            T_haz = float(Thaz_in.get())
-        
+        for i,entry in enumerate(Local_entry):
+            try:
+                float(entry.get())
+            except:
+                messagebox.showerror(title='Type error',message='Enter numbers only')
+                break
+            else:
+                T_holder[param_in[i]]=float(entry.get())
+        Reg = int(reg_var.get())
         if Reg == 1:
-            print('run Y1')
-            y_haz = np.sqrt(weldp['q']*weldp['alpha']/2/np.e/np.pi/weldp['k']/weldp['U'])*(T_m-T_haz)/((weldp['T']-weldp['T_o'])**(3/2))
+            y_haz = np.sqrt(weldp['q']*weldp['alpha']/2/np.e/np.pi/weldp['k']/weldp['U'])*(T_holder['T_m']-T_holder['T_haz'])/((weldp['T']-weldp['T_o'])**(3/2))
+            y_haz=np.round(y_haz,6)
         elif  Reg == 2:
-            y_haz = (weldp['q']/2/np.pi/weldp['k'])*(T_m-T_haz)/(weldp['T']-weldp['T_o'])**2
+            y_haz = (weldp['q']/2/np.pi/weldp['k'])*(T_holder['T_m']-T_holder['T_haz'])/(weldp['T']-weldp['T_o'])**2
+            y_haz=np.round(y_haz,6)
         elif  Reg == 3:
-            y_haz = weldp['q']*weldp['alpha']/np.sqrt(2*np.e*np.pi)/weldp['k']/weldp['d']/weldp['U']*(T_m-T_haz)/(weldp['T']-weldp['T_o'])**2
+            y_haz = weldp['q']*weldp['alpha']/np.sqrt(2*np.e*np.pi)/weldp['k']/weldp['d']/weldp['U']*(T_holder['T_m']-T_holder['T_haz'])/(weldp['T']-weldp['T_o'])**2
+            y_haz=np.round(y_haz,6)
         elif Reg == 4:
             messagebox.showerror(title='error',message='not implemented')
             #gam = float(input('enter gamma '))
             #8*np.pi*weldp['k']*weldp['alpha']*weldp['d']/weldp['q']/weldp['U']*(T_m-T_haz)/np.exp(gam+(1/weldp['Ro']))
-        Yhaz_var.set(str(y_haz))
-        y_window.destroy
+        else:
+            messagebox.showinfo(title='Bad pass',message = 'Reg is passed incorrectly')
+        Yhaz_var.set(y_haz)
+        y_window.destroy()
 
-    y_window = tk.Tk()
-    Tm_in = tk.Entry(y_window).grid(row = 1,column=1)
-    Thaz_in = tk.Entry(y_window).grid(row = 1,column = 2)
-    Tm_label = tk.Label(y_window,text='Melting Temp').grid(row=2,column=1)
-    Thaz_label = tk.Label(y_window,text = 'HAZ temperature').grid(row=2,column=2)
-    Calc_button = tk.Button(y_window,text = 'calculate',command= Ycalc).grid(row=3,column=1)
+    Calc_button = tk.Button(y_window,text='calculate',command=Ycalc).grid(row=3,column=1)
     y_window.mainloop()
-    
+
+
 Yhaz_button = tk.Button(entrywindow, text='Find Y_haz',command=YhazButton).grid(row=9,column=2)
-Yhaz_value = tk.Label(entrywindow,textvariable=Yhaz_var).grid(row=2,column=5)
-Yhaz_label = tk.Label(entrywindow,text='HAZ width: ').grid(row=2,column=4)
+Yhaz_value = tk.Label(entrywindow,textvariable=Yhaz_var).grid(row=2,column=3)
+Yhaz_label = tk.Label(entrywindow,text='HAZ width: ').grid(row=2,column=2)
+
+def YmaxButton():
+    try:
+        int(reg_var.get())
+    except:
+        messagebox.showerror(title='Error',message='Please calculate Regime first')
+    else:
+        Reg = int(reg_var.get())
+        if Reg == 1:
+            y_max = np.sqrt(2*weldp['alpha']*weldp['q']/np.pi/np.e/weldp['k']/weldp['U']/(weldp['T']-weldp['T_o']))
+            y_max=np.round(y_max,6)
+        elif Reg == 2:
+            y_max = weldp['q']/2/np.pi/weldp['k']/(weldp['T']-weldp['T_o'])
+            y_max=np.round(y_max,6)
+        elif Reg == 3:
+            y_max = np.sqrt(1/2/np.pi/np.e)*weldp['q']*weldp['alpha']/weldp['k']/weldp['d']/weldp['U']/(weldp['T']-weldp['T_o'])
+            y_max=np.round(y_max,6)
+        elif Reg == 4:
+            messagebox.showerror(title='error',message='not implemented')
+            #gam = float(input('enter gamma? '))
+            #y_max = 4*np.exp(-gam)*weldp['alpha']/weldp['U']*np.exp(-1/weldp['Ro'])
+            #y_max=np.round(y_max,6)    
+        else:
+            y_max = 'err'
+            messagebox.showerror(title='Bad pass',message='Reg is passed incorrectly')
+        Ymax_var.set(y_max)
+
+
+Ymax_button=tk.Button(entrywindow,text='Find Ymax',command=YmaxButton).grid(row=9,column=3)
+Ymax_value=tk.Label(entrywindow,textvariable=Ymax_var).grid(row=3,column=3)
+Ymax_label=tk.Label(entrywindow,text='Ymax: ').grid(row=3,column=2)
+
+def CoolButton():
+    try:
+        int(reg_var.get())
+    except:
+        messagebox.showerror(title='Error',message='Please calculate Regime first')
+    else:
+        Reg = int(reg_var.get())
+        if Reg == 1 or Reg == 2:
+            T_b = -2*np.pi*weldp['k']*weldp['U']/weldp['q']*(weldp['T']-weldp['T_o'])**2
+            T_b=np.round(T_b,3)
+        elif Reg == 3:
+            T_b = -2*np.pi/weldp['alpha']*((weldp['k']*weldp['d']*weldp['U']/weldp['q'])**2)*(weldp['T']-weldp['T_o'])**3
+            T_b=np.round(T_b,3)    
+        elif Reg == 4:
+            messagebox.showerror(title='error',message='not implemented')
+            #gam = float(input('enter gamma '))
+            #T_b = -weldp['q']/8/np.pi/weldp['k']/weldp['alpha']/weldp['d']*(weldp['U']**2)*np.exp(gam+(1/weldp['Ro']))
+        else:
+            T_b = 'err'
+            messagebox.showerror(title='Bad pass',message='Reg is passed incorrectly')
+        Cool_var.set(T_b)
+
+Cool_button = tk.Button(entrywindow,text='Cool rate',command=CoolButton).grid(row=10,column=2)
+Cool_value = tk.Label(entrywindow,textvariable=Cool_var).grid(row=4,column=3)
+Cool_label =tk.Label(entrywindow,text='Cooling rate: ').grid(row=4,column=2)
+
 entrywindow.mainloop()
